3226411f4157da906650ec54e4758c62dc73d810
Cristian Global <cristianale94sc@gmail.com>
2022-11-21 14:10:34 -0600

    iniciando con la funcion de muestra, y ocultando el contenido

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 
-//document.getElementById("ejercicio1").style.display ="none";
-//document.getElementById("ejercicio2").style.display ="none";
-//document.getElementById("ejercicio3").style.display ="none";
-//document.getElementById("ejercicio4").style.display ="none";
-//document.getElementById("ejercicio5").style.display ="none";
+document.getElementById("ejercicio1").style.display ="none";
+document.getElementById("ejercicio2").style.display ="none";
+document.getElementById("ejercicio3").style.display ="none";
+document.getElementById("ejercicio4").style.display ="none";
+document.getElementById("ejercicio5").style.display ="none";
+
+function seleccionarBandera(){
+    let opcion = document.getElementById("seleccion").value;
+    console.log(opcion);
+}
