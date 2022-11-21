7eff06b001f69e4acc79d21f57d6ab4fcd13180e
Cristian Global <cristianale94sc@gmail.com>
2022-11-21 13:41:18 -0600

    pruevas en js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+//document.getElementById("ejercicio1").style.display ="none";
+//document.getElementById("ejercicio2").style.display ="none";
+//document.getElementById("ejercicio3").style.display ="none";
+//document.getElementById("ejercicio4").style.display ="none";
+//document.getElementById("ejercicio5").style.display ="none";
